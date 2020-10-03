3c820e45351f2ffd4ec407a9e794845ea14a44a5
a1barif2h <a1barif2h@gmail.com>
2020-10-03 18:15:16 +0600

    working dependancy is rady

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const addVolunteerTask = () => {
+  //   fetch("http://localhost:5000/addVolunteerTask", {
+  //     method: "POST",
+  //     headers: { "Content-type": "application/json" },
+  //     body: JSON.stringify(volunteerEvent),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // };
+
+  const [volunteerTasks, setVolunteerTasks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/volunteerTasks")
+      .then((res) => res.json())
+      .then((data) => setVolunteerTasks(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {volunteerTasks.map((task) => (
+        <div>
+          <p>{task.title}</p>
+          <img src={task.pic} alt={task.title} />
+        </div>
+      ))}
     </div>
   );
 }
